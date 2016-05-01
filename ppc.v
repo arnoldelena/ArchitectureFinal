f94b762a98581432f838dd12968875161dc2da8e
Seth Taylor <seth11@cs.utexas.edu>
2016-04-30 20:42:25 -0500

    I give up.

--- ppc.v
+++ ppc.v
@@ -11,7 +11,6 @@ module main();
 
     clock clock0(halt,clk);
 
-    reg[0:63] oldpc = 0;
     reg[0:63] pc = 0;
     reg[0:63] lr = 0;
     reg[0:63] ctr = 0;
@@ -82,26 +81,55 @@ module main();
     );
 
     reg[0:63][0:31] queue = 0;
-    reg[0:5] pointer = 0;
+    reg[0:5] tail = 0;
+    reg[0:5] head = 0;
 
     /*********/
     /* Fetch */
     /*********/
 
-    wire[0:63] fetch = ~state ? 0 : memReadData0;
+    wire[0:63] fetch = ~state ? 0 : memReadData0; // Might want to stop fetching at some point.
 
-    queue[pointer] = memReadData0[0:31];
-    queue[pointer + 1] = memReadData0[32:63];
+    /**********/
+    /* Decode */
+    /**********/
+
+    reg[0:31] Dinst0 = 0;
+    reg[0:31] Dinst1 = 0;
+
+    /************/
+    /* Exectute */
+    /************/
+
+    reg[0:31] Xinst0 = 0;
+    reg[0:31] Xinst1 = 0;
+
+    /**************/
+    /* Write Back */
+    /**************/
+
+    reg[0:31] WBinst0 = 0;
+    reg[0:31] WBinst1 = 0;
 
     /**********/
     /* Update */
     /**********/
 
-    wire[0:5] nextPointer;
+    wire[0:5] nextHead = canParallel ? head + 2 : head + 1;
+    wire[0:5] nextTail = state ? tail + 2 : tail;
     wire[0:63] pcPlus4 = pc + 4;
     wire[0:63] nextpc = pcPlus4;
     always @(posedge clk) begin
-        pointer <= nextPointer;
+        queue[tail + 1] = fetch[32:63];
+        queue[tail] = fetch[0:31];
+        WBinst1 <= Xinst1;
+        WBinst0 <= Xinst0;
+        Xinst1 <= Dinst1;
+        Xinst0 <= Dinst0;
+        Dinst1 <= queue[head + 1];
+        Dinst0 <= queue[head];
+        head <= nextHead;
+        tail <= nextTail;
         pc <= nextpc;
         state <= 1;
     end
